392ce9f181d527b0ab1701bb051b771c9a776873
Sooraj Soman <soorajsomans@gmail.com>
2021-11-30 20:24:28 +0530

    Adding http post request for adding products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'product.dart';
+import 'package:http/http.dart' as http;
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -65,15 +67,29 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
-    final newProduct = Product(
+    const url =
+        'https://shoppingapp-c85a1-default-rtdb.asia-southeast1.firebasedatabase.app/products.json';
+    http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'price': product.price,
+              'imageUrl': product.imageUrl,
+              'isFavorite': product.isFavorite
+            }))
+        .then((response) {
+      final newProduct = Product(
         title: product.title,
         description: product.description,
         price: product.price,
         imageUrl: product.imageUrl,
-        id: DateTime.now().toString());
+        id: json.decode(response.body)['name'],
+      );
 
-    _items.add(newProduct);
-    notifyListeners();
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
